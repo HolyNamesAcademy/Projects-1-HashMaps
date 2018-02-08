b4b452356adf6a4480c58aeff937d5e4a397127b
Brian Golden <brian.golden09@gmail.com>
2018-02-08 14:22:05 -0800

    throw exception with descriptive message when functions have not been implemented yet

--- src/main/java/main/CompanyDirectoryArrayList.java
+++ src/main/java/main/CompanyDirectoryArrayList.java
@@ -11,27 +11,27 @@ public class CompanyDirectoryArrayList implements ICompanyDirectory {
 
     public void addEmployee(Employee employee) {
         // TODO: implement this
-        throw new UnsupportedOperationException();
+        throw new UnsupportedOperationException("CompanyDirectoryArrayList.addEmployee() has not been implemented yet");
     }
 
     public boolean addOrReplaceEmployee(Employee employee) {
         // TODO: implement this
-        throw new UnsupportedOperationException();
+        throw new UnsupportedOperationException("CompanyDirectoryArrayList.addOrReplaceEmployee() has not been implemented yet");
     }
 
     public Employee findEmployeeByName(String contactName) {
         // TODO: implement this
-        throw new UnsupportedOperationException();
+        throw new UnsupportedOperationException("CompanyDirectoryArrayList.findEmployeeByName() has not been implemented yet");
     }
 
     public Employee findEmployeeByOffice(String buildingName, int officeNumber) {
         // TODO: implement this
-        throw new UnsupportedOperationException();
+        throw new UnsupportedOperationException("CompanyDirectoryArrayList.findEmployeeByOffice() has not been implemented yet");
     }
 
     public void DisplayAllEmployees() {
         // TODO: implement this
-        throw new UnsupportedOperationException();
+        throw new UnsupportedOperationException("CompanyDirectoryArrayList.DisplayAllEmployees() has not been implemented yet");
     }
 
     public ArrayList<Employee> getAllEmployees() {
--- src/main/java/main/CompanyDirectoryHashMap.java
+++ src/main/java/main/CompanyDirectoryHashMap.java
@@ -11,27 +11,27 @@ public class CompanyDirectoryHashMap implements ICompanyDirectory {
 
     public void addEmployee(Employee employee) {
         // TODO: implement this
-        throw new UnsupportedOperationException();
+        throw new UnsupportedOperationException("CompanyDirectoryHashMap.addEmployee() has not been implemented yet");
     }
 
     public boolean addOrReplaceEmployee(Employee employee) {
         // TODO: implement this
-        throw new UnsupportedOperationException();
+        throw new UnsupportedOperationException("CompanyDirectoryHashMap.addOrReplaceEmployee() has not been implemented yet");
     }
 
     public Employee findEmployeeByName(String contactName) {
         // TODO: implement this
-        throw new UnsupportedOperationException();
+        throw new UnsupportedOperationException("CompanyDirectoryHashMap.findEmployeeByName() has not been implemented yet");
     }
 
     public Employee findEmployeeByOffice(String buildingName, int officeNumber) {
         // TODO: implement this
-        throw new UnsupportedOperationException();
+        throw new UnsupportedOperationException("CompanyDirectoryHashMap.findEmployeeByOffice() has not been implemented yet");
     }
 
     public void DisplayAllEmployees() {
         // TODO: implement this
-        throw new UnsupportedOperationException();
+        throw new UnsupportedOperationException("CompanyDirectoryHashMap.DisplayAllEmployees() has not been implemented yet");
     }
 
     public HashMap<String, Employee> getAllEmployees() {
