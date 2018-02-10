9c052495310f9d8139865bd7ff5fe811b4e57257
Brian Golden <brian.golden09@gmail.com>
2018-02-10 12:34:06 -0800

    add inline descriptions of tests

--- src/test/java/test/CompanyDirectoryArrayListTests.java
+++ src/test/java/test/CompanyDirectoryArrayListTests.java
@@ -8,20 +8,30 @@ import static org.junit.Assert.*;
 
 public class CompanyDirectoryArrayListTests {
 
+    // Test that you can successfully add a new employee to the directory.
     @Test
-    public void addContact() {
+    public void addContact() throws Exception {
         // TODO: implement this
         throw new UnsupportedOperationException();
     }
 
+    // Add a new employee to the directory and find that employee by their name.
     @Test
-    public void findEmployeeByName() {
+    public void findEmployeeThatExistsByName() throws Exception {
         // TODO: implement this
         throw new UnsupportedOperationException();
     }
 
+    // Find an employee that is not in the directory by their name.
     @Test
-    public void findEmployeeByOffice() {
+    public void findEmployeeThatDoesNotExistByName() throws Exception {
+        // TODO: implement this
+        throw new UnsupportedOperationException();
+    }
+
+    // Add a new employee to the directory and find that employee by their office.
+    @Test
+    public void findEmployeeByOffice() throws Exception {
         // TODO: implement this
         throw new UnsupportedOperationException();
     }
